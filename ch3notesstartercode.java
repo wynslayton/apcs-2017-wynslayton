e827027fca8bb8a0b8e6f906456926b7968f5e0a
wynslayton <31413227+wynslayton@users.noreply.github.com>
2017-09-08 09:55:06 -0500

    new starter code

---
+++ ch3notesstartercode.java
@@ -0,0 +1,55 @@
+/********************************************************************
+// These are the notes for ch 3 used in hayes class
+
+
+import
+//******************************************************************* */
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+//import cs1.Keyboard;
+import javax.swing.*;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+
+public class ch3notesstartercode
+{
+public static void main (String [] args)
+{
+
+/* ===============================================================
+	   String word = "apple";
+		String word1 = "cat";
+		String word2 = "dog";
+
+/*
+
+      int num = 5, num1 =10, num2 = 4;  // standard hours in a work week
+
+      System.out.print ("Enter a number ");
+      num = Keyboard.readInt();
+
+       System.out.print ("Enter a  second number ");
+	   num1 = Keyboard.readInt();
+
+       System.out.print ("Enter a  third number ");
+	   num2 = Keyboard.readInt();
+
+      System.out.println ();
+
+
+/*  */
+
+}// end of main
+
+
+}// end of class
+
+
+
+
+
+
+
+
+
