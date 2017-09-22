5fcf87a0368dd7eb2a6d9c121950b3467f915724
wynslayton <31413227+wynslayton@users.noreply.github.com>
2017-09-22 09:33:54 -0500

    e

--- Ch3SoloProgs/TriangleProg.java
+++ Ch3SoloProgs/TriangleProg.java
@@ -1,40 +1,14 @@
+//Wynant Slayton
+//Mr Hayes
+//9/22/2017
+//This program will take any 3 sides or angles of a triangle and return what kind of a triangle it is
 
-
-/**
- * Write a description of class TriangleProg here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import java.util.*;
 public class TriangleProg
 {
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
-
-    /**
-     * Default constructor for objects of class TriangleProg
-     */
     public TriangleProg()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x+y;
-    }
-
-}
+    
+        
+    }//end of main
+}//end of class
