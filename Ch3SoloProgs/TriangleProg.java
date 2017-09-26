43a9a944030c72c233d9f4b561c5af436d87dbdd
wynslayton <31413227+wynslayton@users.noreply.github.com>
2017-09-26 09:32:47 -0500

    e

--- Ch3SoloProgs/TriangleProg.java
+++ Ch3SoloProgs/TriangleProg.java
@@ -8,7 +8,32 @@ public class TriangleProg
 {
     public TriangleProg()
     {
-    
+        Scanner scan = new Scanner(System.in);
+        double a, b, c;
+        String str = new String();
+        System.out.println("Would you like to test in angles (A) or side lengths (S)? ");
+        str = scan.nextLine();
+        System.out.println("Please enter your 3 values");
+        System.out.print("Value 1:");
+        a = scan.nextDouble();
+        System.out.print("\nValue 2:");
+        b = scan.nextDouble();
+        System.out.print("\nValue 3:");
+        c = scan.nextDouble();
+        
+        if (str == "A"){
+            if ((a + b + c) == 180.0){
+                System.out.println("It works!");
+            }
+            else{System.out.println("It doesn't work, you're bad");}
+        }
+        else{
+            if (((a + b) > c) && ((a + c) > b) && ((b + c) > a)){
+                System.out.println("It works!");
+            }
+            else{System.out.println("It doesn't work, you're bad");}
+        }
+        
         
     }//end of main
 }//end of class
