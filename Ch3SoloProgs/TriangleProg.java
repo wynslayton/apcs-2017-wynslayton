a1bd401be8fe00a978dd716d7c2adc96e6d1babe
wynslayton <31413227+wynslayton@users.noreply.github.com>
2017-09-27 10:24:31 -0500

    yee

--- Ch3SoloProgs/TriangleProg.java
+++ Ch3SoloProgs/TriangleProg.java
@@ -6,14 +6,13 @@
 import java.util.*;
 public class TriangleProg
 {
-    public TriangleProg()
+    public static void main( String [] args)
     {
         Scanner scan = new Scanner(System.in);
-        double a, b, c;
-        String str = new String();
-        System.out.println("Would you like to test in angles (A) or side lengths (S)? ");
-        str = scan.nextLine();
-        System.out.println("Please enter your 3 values");
+        double a, b, c, choice;
+        System.out.println("Would you like to test in angles (1) or side lengths (2)? ");
+        choice = scan.nextDouble();
+        System.out.println("\nPlease enter your 3 values");
         System.out.print("Value 1:");
         a = scan.nextDouble();
         System.out.print("\nValue 2:");
@@ -21,17 +20,17 @@ public class TriangleProg
         System.out.print("\nValue 3:");
         c = scan.nextDouble();
         
-        if (str == "A"){
+        if (choice == 1.0){
             if ((a + b + c) == 180.0){
-                System.out.println("It works!");
+                System.out.println("\nIt works!");
             }
-            else{System.out.println("It doesn't work, you're bad");}
+            else{System.out.println("\nIt doesn't work, you're bad");}
         }
         else{
             if (((a + b) > c) && ((a + c) > b) && ((b + c) > a)){
-                System.out.println("It works!");
+                System.out.println("\nIt works!");
             }
-            else{System.out.println("It doesn't work, you're bad");}
+            else{System.out.println("\nIt doesn't work, you're bad");}
         }
         
         
