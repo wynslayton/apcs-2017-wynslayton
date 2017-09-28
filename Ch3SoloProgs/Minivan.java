4ae89228ee1060ead6080a6a22d069829006b21d
wynslayton <31413227+wynslayton@users.noreply.github.com>
2017-09-28 09:31:35 -0500

    e

--- Ch3SoloProgs/Minivan.java
+++ Ch3SoloProgs/Minivan.java
@@ -1,40 +1,46 @@
+//Wynant Slayton
+//APCS p 2
+//9/28/2017
+//This program will test to see whether the doors on a minivan will be open or not
+import java.util.*;
+public class Minivan{ 
+    public static void main (String [] args){
+        char dl, dr, cl, mu, il, ir, ol, or, gear;
+        int right, left;
+        Scanner scan = new Scanner(System.in);
+        String input = new String("");
+        
+        System.out.println("Please enter in the variables next to each other in the order of driver left, right, child lock, master unlock, inside left, inside right, outside left, outside right, and the gear.");
+        
+        input = scan.nextLine();
+        
+        dl = input.charAt(0);
+        dr = input.charAt(1);
+        cl = input.charAt(2);
+        mu = input.charAt(3);
+        il = input.charAt(4);
+        ir = input.charAt(5);
+        ol = input.charAt(6);
+        or = input.charAt(7);
+        gear = input.charAt(8);
+        
+        if (gear == "P"){
+            if (mu == 1){
+                if (cl == 0){
+                    if (dl == 1 || il == 1 || ol == 1)
+                }
+                else{
+                    
+                }
+            }
+            else{
+                System.out.println("\nMaster unlock off.");
+            }
+        }
+        else{
+            System.out.println("\nNot in park.");
+        }
+    
+    }//end of main
+}//end of class
 
-
-/**
- * Write a description of class Minivan here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Minivan
-{
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
-
-    /**
-     * Default constructor for objects of class Minivan
-     */
-    public Minivan()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x+y;
-    }
-
-}
