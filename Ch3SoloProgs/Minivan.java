4e41dfdaa2e464576d3482896c4a14fba6e15fe8
wynslayton <31413227+wynslayton@users.noreply.github.com>
2017-09-28 22:42:11 -0500

    e

--- Ch3SoloProgs/Minivan.java
+++ Ch3SoloProgs/Minivan.java
@@ -6,31 +6,52 @@ import java.util.*;
 public class Minivan{ 
     public static void main (String [] args){
         char dl, dr, cl, mu, il, ir, ol, or, gear;
-        int right, left;
         Scanner scan = new Scanner(System.in);
-        String input = new String("");
+        String inp = new String("");
         
         System.out.println("Please enter in the variables next to each other in the order of driver left, right, child lock, master unlock, inside left, inside right, outside left, outside right, and the gear.");
         
-        input = scan.nextLine();
+        inp = scan.nextLine();
         
-        dl = input.charAt(0);
-        dr = input.charAt(1);
-        cl = input.charAt(2);
-        mu = input.charAt(3);
-        il = input.charAt(4);
-        ir = input.charAt(5);
-        ol = input.charAt(6);
-        or = input.charAt(7);
-        gear = input.charAt(8);
+        dl = inp.charAt(0);
+        dr = inp.charAt(1);
+        cl = inp.charAt(2);
+        mu = inp.charAt(3);
+        il = inp.charAt(4);
+        ir = inp.charAt(5);
+        ol = inp.charAt(6);
+        or = inp.charAt(7);
+        gear = inp.charAt(8);
         
-        if (gear == "P"){
+        if (gear == 'P'){
             if (mu == 1){
                 if (cl == 0){
-                    if (dl == 1 || il == 1 || ol == 1)
+                    if (dl == 1 || il == 1 || ol == 1){
+                        System.out.println("Left door open.");
+                    }
+                    else{
+                        System.out.println("Left door closed.");
+                    }
+                    if (dr == 1 || ir == 1 || or == 1){
+                        System.out.println("Right door open.");
+                    }
+                    else{
+                        System.out.println("Left door closed.");
+                    }
                 }
                 else{
-                    
+                    if (dl == 1 || ol == 1){
+                        System.out.println("Left door open.");
+                    }
+                    else{
+                        System.out.println("Left door closed.");
+                    }
+                    if (dr == 1 || or == 1){
+                        System.out.println("Right door open.");
+                    }
+                    else{
+                        System.out.println("Left door closed.");
+                    }
                 }
             }
             else{
@@ -40,7 +61,6 @@ public class Minivan{
         else{
             System.out.println("\nNot in park.");
         }
-    
     }//end of main
 }//end of class
 
---
+++ Ch3SoloProgs/__SHELL7.java
@@ -0,0 +1,8 @@
+
+public class __SHELL7 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+Minivan.main(__bluej_param0);
+
+}}
